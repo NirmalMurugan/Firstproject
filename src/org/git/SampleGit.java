9cb1b0a1cf4a1ccfd8357b36a90d02360f1ebf92
NirmalMurugan <nirmalmurugan001@gmail.com>
2022-03-27 11:47:33 +0530

    Message

---
+++ src/org/git/SampleGit.java
@@ -0,0 +1,12 @@
+package org.git;
+
+public class SampleGit {
+private void Sample() {
+System.out.println("samp");
+
+}
+public static void main(String[] args) {
+	SampleGit s=new SampleGit();
+	s.Sample();
+}
+}
